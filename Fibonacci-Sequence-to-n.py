d653c0b1910717b432270a095589870a0f3a4989
mukund0299 <mukund0299@gmail.com>
2017-11-28 10:32:45 -0800

    Added Fibonacci Sequence problem

---
+++ Fibonacci-Sequence-to-n.py
@@ -0,0 +1,14 @@
+def fib(n):
+    numList = []
+    curr = 1
+    prev = 0
+    while len(numList)<n:
+        numList.append(curr)
+        curr, prev = curr + prev, curr
+    print(*numList, sep='\n')
+
+n = int(input('Enter number of digits'))
+fib(n)
+
+
+
